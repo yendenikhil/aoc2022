de92357a3124147e39fb95c535fe6068de3a22dc
Nikhil Yekhe <nikhil.yekhe@ing.com>
2022-12-04 21:29:38 +0100

    day 4 - refactoring

--- day4.ts
+++ day4.ts
@@ -1,23 +1,24 @@
 const p = console.log;
 const raw = await Deno.readTextFile("4.in");
 
-const part1 = (lines: string[]) => {
-  const tokens = lines
+const tokens = (lines: string[]) => {
+  return lines
     .map((line) => line.match(/(\d+)-(\d+),(\d+)-(\d+)/))
     .map((
       [zz, a, b, c, d],
-    ) => [parseInt(a), parseInt(b), parseInt(c), parseInt(d)])
-    .filter(([a, b, c, d]) => ((a <= c && b >= d) || (a >= c && b <= d)));
-  p(tokens.length);
+    ) => [parseInt(a), parseInt(b), parseInt(c), parseInt(d)]);
+};
+
+const part1 = (lines: string[]) => {
+  const p1 = tokens(lines).filter((
+    [a, b, c, d],
+  ) => ((a <= c && b >= d) || (a >= c && b <= d)));
+  p(p1.length);
 };
 const part2 = (lines: string[]) => {
-  const tokens = lines
-    .map((line) => line.match(/(\d+)-(\d+),(\d+)-(\d+)/))
-    .map((
-      [zz, a, b, c, d],
-    ) => [parseInt(a), parseInt(b), parseInt(c), parseInt(d)])
-    .filter(([a, b, c, d]) => !((b < c) || (a > d)));
-  p(tokens.length);
+  const p2 = tokens(lines)
+    .filter(([a, b, c, d]) => b >= c && a <= d);
+  p(p2.length);
 };
 
 part1(raw.trim().split("\n"));
